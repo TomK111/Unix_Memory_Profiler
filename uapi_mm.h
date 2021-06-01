5a4f9f3c46324b8e3e4694d546c400717e329461
tomislavkraljic <tomislavkraljic@outlook.com>
2021-06-01 00:54:19 -0400

    "finished project"

---
+++ uapi_mm.h
@@ -0,0 +1,26 @@
+#include <stdint.h>
+
+void * xcalloc(char *struct_name, int units);
+
+void xfree(void *app_ptr);
+
+void mm_instantiate_new_page_family(char *struct_name, uint32_t struct_size);
+
+/*Printing Functions*/
+void mm_print_memory_usage(char *struct_name);
+
+void mm_print_block_usage();
+
+/*Initialization Functions*/
+void mm_init();
+
+/*Registration function*/
+#define MM_REG_STRUCT(struct_name)  \
+    (mm_instantiate_new_page_family(#struct_name, sizeof(struct_name)))
+
+/*Allocators and De-Allocators*/
+#define XCALLOC(units, struct_name) \
+    (xcalloc(#struct_name, units))
+
+#define XFREE(ptr)  \
+    xfree(ptr)
